9e1cf5ef8486335aeb13478f12aa92ba5e415c31
moscams <89820427+moscams@users.noreply.github.com>
2025-10-26 22:03:30 +0800

    Change applicationId from 'com.v2ray.ang' to 'com.network.testray'

--- V2rayNG/app/build.gradle.kts
+++ V2rayNG/app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.v2ray.ang"
+        applicationId = "com.network.testray"
         minSdk = 21
         targetSdk = 35
         versionCode = 677
